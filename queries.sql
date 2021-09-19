dba4c4aa620830c7bafa121e898fc3872797293e
olawale-o <omoogunolawale@gmail.com>
2021-09-19 20:42:30 +0100

    Modify queries.sql

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 -- Find all animals whose name ends in "mon".
-SELECT * from animals WHERE name LIKE 'mon%';
+SELECT * FROM animals WHERE name LIKE '%mon';
 
 -- List the name of all animals born between 2016 and 2019.
 SELECT id, name, date_of_birth, neutered, escape_attempts, date_of_birth, weight_kg FROM
