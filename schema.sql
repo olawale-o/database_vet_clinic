3697cd10dd774d394b3155229619fc09e60af241
olawale-o <omoogunolawale@gmail.com>
2021-09-20 11:53:56 +0100

    Create animals table in vet_clinic database

--- schema.sql
+++ schema.sql
@@ -2,3 +2,11 @@
 
 CREATE DATABASE vet_clinic;
 
+CREATE TABLE animals (
+  id int UNIQUE PRIMARY KEY,
+  name VARCHAR(100),
+  date_of_birth date,
+  escape_attempts int,
+  neutered BOOLEAN DEFAULT false,
+  weight_kg decimal(10,4)
+);
