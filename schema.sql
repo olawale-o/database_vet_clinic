5b6bfc08b9049883218c6c5112e832fce0ea928d
olawale-o <omoogunolawale@gmail.com>
2021-09-20 13:51:26 +0100

    Modify schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
+-- Milestone: Vet clinic database: create animals table
+
 CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals (
@@ -10,5 +12,7 @@ CREATE TABLE animals (
   neutered BOOLEAN DEFAULT false,
   weight_kg decimal(10,4)
 );
+
+-- Milestone: Vet clinic database: query and update animals table
 
 ALTER TABLE animals ADD species VARCHAR(100);
