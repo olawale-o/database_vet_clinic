e0c75476e4d4910474f22216ec5d8d382909df2f
olawale-o <omoogunolawale@gmail.com>
2021-09-20 15:56:35 +0100

    Create owners table

--- schema.sql
+++ schema.sql
@@ -16,3 +16,16 @@ CREATE TABLE animals (
 -- Milestone: Vet clinic database: query and update animals table
 
 ALTER TABLE animals ADD species VARCHAR(100);
+
+-- Vet clinic database: query multiple tables
+
+-- Create a table named owners with the following columns:
+-- id: integer (set it as autoincremented PRIMARY KEY)
+-- full_name: string
+-- age: integer
+
+CREATE TABLE owners (
+  id int UNIQUE GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  full_name VARCHAR(100),
+  age INT
+);
