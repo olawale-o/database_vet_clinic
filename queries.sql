1d4c036dc62525c5a56294f611263435e511cc01
olawale-o <omoogunolawale@gmail.com>
2021-09-20 14:51:47 +0100

    Add project transaction requirements in queries.sql file

--- queries.sql
+++ queries.sql
@@ -60,3 +60,44 @@ SELECT AVG(escape_attempts), neutered FROM animals WHERE EXTRACT(YEAR FROM date_
 -- ---+----------
 --  3.0 | f
 --  3.0 | t
+
+-- BELOW IS THE TRANSACTION REQUIREMENTS FOR THE PROJECT 
+
+-- Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that species columns went back to the state before tranasction.
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+-- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+-- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+
+-- Now, take a deep breath and... Inside a transaction delete all records in the animals table, then roll back the transaction.
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+-- After the roll back verify if all records in the animals table still exist. After that you can start breath as usual ;)
+SELECT * FROM animals;
+
+-- Inside a transaction:
+-- Delete all animals born after Jan 1st, 2022.
+-- Create a savepoint for the transaction.
+-- Update all animals' weight to be their weight multiplied by -1.
+-- Rollback to the savepoint
+-- Update all animals' weights that are negative to be their weight multiplied by -1.
+-- Commit transaction
+
+BEGIN;
+SAVEPOINT ANIMALS_BORN_AFTER_2022_01_01;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+UPDATE animals SET weight_kg = (-1 * weight_kg);
+ROLLBACK TO SAVEPOINT ANIMALS_BORN_AFTER_2022_01_01;
+UPDATE animals SET weight_kg = (-1 * weight_kg) WHERE weight_kg < 0;
+COMMIT;
